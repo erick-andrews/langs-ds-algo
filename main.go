77e9679319c2701ecf08be52c1b696fbfd112868
Erick Andrews <andrews_erick@yahoo.com>
2025-03-03 15:07:36 +0000

    adding more vars, declare const

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 
 	fmt.Println("Hello!")
 	fmt.Println("Welcome to", conferenceName, "booking application")
+
+	fmt.Println("The number of tickets available is:", remainingTickets)
+	fmt.Println("The number of tickets left is:", conferenceTickets)
 }
