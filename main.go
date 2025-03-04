32df85c56be20c0619ba49c7f57d643ef912cfec
Erick Andrews <andrews_erick@yahoo.com>
2025-03-04 15:56:40 +0000

    adding pointer arg for inplace update

--- main.go
+++ main.go
@@ -11,8 +11,10 @@ func main() {
 	fmt.Printf("Welcome to %v booking application \n", conferenceName)
 	//Let's see the types of the above vars:
 	fmt.Printf("conferenceTickets is %T, conferenceName is %T \n", conferenceTickets, conferenceName)
-
-	remainingTickets = askUserNameTicketsNum(remainingTickets)
+	var bookings []string
+	for {
+		remainingTickets = askUserNameTicketsNum(&bookings, remainingTickets)
+	}
 
 	//fmt.Printf("The number of tickets available is: %v and %v is the total left. \n", remainingTickets, conferenceTickets)
 	fmt.Println("The number of tickets left is:", remainingTickets)
@@ -23,7 +25,7 @@ func main() {
 
 // ints: uint8-64, and int8-64. uint prohibits neg, int is neg to pos.
 
-func askUserNameTicketsNum(remainingTickets uint) uint {
+func askUserNameTicketsNum(bookings *[]string, remainingTickets uint) uint {
 	var firstName string
 	var lastName string
 	var email string
@@ -36,6 +38,8 @@ func askUserNameTicketsNum(remainingTickets uint) uint {
 	fmt.Println("Enter your last name:")
 	fmt.Scan(&lastName)
 
+	createArrays(bookings, firstName, lastName)
+
 	fmt.Println("Enter your email:")
 	fmt.Scan(&email)
 
@@ -47,6 +51,24 @@ func askUserNameTicketsNum(remainingTickets uint) uint {
 	fmt.Printf("User %v %v booked %v tickets.\n", firstName, lastName, userTickets)
 
 	// fmt.Printf("Remaining tickets: %v", remainingTickets)
-
 	return remainingTickets // Return the updated value
+}
+
+func createArrays(bookings *[]string, firstName string, lastName string) {
+	// Arrays in Go are of a fixed size
+	// What kind of value do we want to store here? A list of names of users who booked tickets!
+	// Can be empty or already have things in it.
+	// No mixed types allowed in an array.
+
+	// var bookings = [50]string{"Erick", "Tom", "Pete"}
+	// var bookings [50]string
+
+	// But what about dynamic size list? A slice - array type under hood, but is dynamic in size.
+	*bookings = append(*bookings, firstName+" "+lastName)
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(*bookings))
+
+	fmt.Printf("These are all our bookings: %v\n", bookings)
+
 }
