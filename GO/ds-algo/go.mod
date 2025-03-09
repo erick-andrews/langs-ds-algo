04032f2f339bd14c81e366d2e095910b4dff46c1
Erick Andrews <andrews_erick@yahoo.com>
2025-03-09 19:34:07 +0000

    start ds-algo w go

---
+++ GO/ds-algo/go.mod
@@ -0,0 +1,3 @@
+module interview_q1
+
+go 1.24.1
---
+++ GO/ds-algo/interview_q1/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+)
+
+var a1 = []string{"a", "b", "c", "d"}
+var a2 = []string{"z", "y", "i"}
+
+var a3 = []string{"a", "b", "c", "d", "x"}
+var a4 = []string{"z", "y", "x"}
+
+func main() {
+	containsOverlap := containsCommonItem(a3, a4)
+	fmt.Printf("These arrays contain an overlap: %v\n", containsOverlap)
+
+}
+
+// func containsCommonItem(array1 []string, array2 []string) bool {
+// 	// brute force O^2 nested for loop - not ideal, but start with brute force.
+// 	// Naive, bad approach, but baseline nonetheless.
+// 	// declare boolean return
+// 	overlap := false
+// 	for _, str1 := range array1 {
+// 		for _, str2 := range array2 {
+// 			if str1 == str2 {
+// 				overlap := true
+// 				return overlap
+// 			}
+// 		}
+// 	}
+// 	return overlap
+// }
+
+func containsCommonItem(array1 []string, array2 []string) bool {
+	// this solution has O(a + b) time complexity.
+	// Faster than above.
+	// declare boolean return
+	overlap := false
+	// thinking... array to map. O(n)
+	//array1 -> object{
+	// a:true
+	// b:true
+	//c: true
+	//x: true
+	//}
+	// array2[index] == obj.properties
+
+	var array1Map = make(map[string]bool, len(array1))
+	// loop through array and create map from array w/ bools
+	for _, key := range array1 {
+		array1Map[key] = true
+	}
+	// loop through second array and check if second array exists on created object
+	for _, element := range array2 {
+		if array1Map[element] {
+			overlap := true
+			return overlap
+		}
+	}
+	return overlap
+}
+
+func containsCommonItemGeneric[T comparable](array1 []T, array2 []T) bool {
+	// Take in our array1, make map
+	array1Map := make(map[T]bool, len(array1))
+	for _, key := range array1 {
+		array1Map[key] = true
+	}
+	// compare array2 to map.
+	for _, element := range array2 {
+		if array1Map[element] {
+			return true
+		}
+	}
+
+	return false
+
+}
