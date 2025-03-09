762231eb23043d9d6421917a56dd9ba33f182db8
Erick Andrews <andrews_erick@yahoo.com>
2025-03-09 16:43:49 +0000

    added note to helper about package imports

--- helper/helper.go
+++ helper/helper.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 )
 
+// moving things to another subfolder to experiment with package imports!
+
 func ValidateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) bool {
 	isValidName := len(firstName) >= 1 && len(lastName) >= 1
 	isValidEmail := strings.Contains(email, "@")
